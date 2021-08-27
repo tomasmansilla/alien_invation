ad85f0f16ec0396d8943e71dd1ed96fb0d8d7a4f
tomasmansillautn <79302178+tomasmansillautn@users.noreply.github.com>
2021-08-27 13:08:39 -0300

    all working

--- alien_invation.py
+++ alien_invation.py
@@ -4,6 +4,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
@@ -22,6 +23,7 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -74,6 +76,12 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+    def _create_fleet(self):
+        """Create the fleet of aliens"""
+        # Make an alien
+        alien = Alien(self)
+        self.aliens.add(alien)
 
     def _update_screen(self):
         """Updates images to the screen, and flip to the new screen."""
