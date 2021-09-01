805d2d229325aa17786c9efa54e33423c0cdcbc7
tomasmansillautn <79302178+tomasmansillautn@users.noreply.github.com>
2021-09-01 16:54:50 -0300

    added speed

--- alien_invation.py
+++ alien_invation.py
@@ -133,6 +133,7 @@ class AlienInvasion:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _update_aliens(self):
         """
