07b0fa3c82e2f1634a91f3d53b67590f5a9f122a
tomasmansillautn <79302178+tomasmansillautn@users.noreply.github.com>
2021-09-02 01:25:17 -0300

    added high score

--- alien_invation.py
+++ alien_invation.py
@@ -161,6 +161,7 @@ class AlienInvasion:
             for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
+            self.sb.chech_high_score()
 
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
