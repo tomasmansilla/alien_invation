ce7323396c20e3ceef3d31298f13c10b05989170
tomasmansillautn <79302178+tomasmansillautn@users.noreply.github.com>
2021-09-02 01:07:38 -0300

    fixing

--- alien_invation.py
+++ alien_invation.py
@@ -158,7 +158,7 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
         if collisions:
-            for aliens in collisions.values()
+            for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
 
