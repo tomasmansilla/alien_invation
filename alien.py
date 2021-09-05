f6f9ec771939d480dc43890cb50db8da513c5f95
tomasmansillautn <79302178+tomasmansillautn@users.noreply.github.com>
2021-09-05 14:54:09 -0300

    refactoring

--- alien.py
+++ alien.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame.sprite import Sprite
-from random import randint
+from random import choice
+
+from bullet import Bullet
 
 
 class Alien(Sprite):
@@ -11,13 +13,15 @@ class Alien(Sprite):
         super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
+        self.screen_rect = self.screen.get_rect()
 
         # load the image and set its rect attribute.
-        random_number = randint(1, 3)
-        if random_number == 1:
+        alien_variations = ['red', 'green', 'purple']
+        alien_random = choice(alien_variations)
+        if alien_random == 'green':
             self.image = pygame.image.load("images/alien.bmp")
             self.rect = self.image.get_rect()
-        elif random_number == 2:
+        elif alien_random == 'red':
             self.image = pygame.image.load("images/alien_red.bmp")
             self.rect = self.image.get_rect()
         else:
@@ -33,8 +37,7 @@ class Alien(Sprite):
 
     def check_edges(self):
         """Return True if an alien is at the edge of the screen."""
-        screen_rect = self.screen.get_rect()
-        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+        if self.rect.right >= self.screen_rect.right or self.rect.left <= 0:
             return True
 
     def update(self):
