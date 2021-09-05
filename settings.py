1a0ecdf12226990add523bf73a89cdc687796c7c
tomasmansillautn <79302178+tomasmansillautn@users.noreply.github.com>
2021-09-05 14:55:41 -0300

    fixing

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ class Settings:
         self.ship_limit = 3
 
         # Bullets settings.
-        self.bullet_width = 1000
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
